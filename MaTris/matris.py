7ba34d6acdcb01d8e64830d9a60b45b2efba0619
Wukai <1183323265@qq.com>
2014-07-25 16:43:58 +0800

    2014_07_25 commit

--- MaTris/matris.py
+++ MaTris/matris.py
@@ -7,7 +7,7 @@ import kezmenu
 from tetrominoes import list_of_tetrominoes
 from tetrominoes import rotate
 
-from scres import load_score, write_score
+from scores import load_score, write_score
 
 class BrokenMatrixException(Exception):
     pass
@@ -35,7 +35,7 @@ VISIBLE_MATRIX_HEIGHT = MATRIX_HEIGHT - 2
 class Matris(object):
     def __init__(self, size=(MATRIX_WIDTH, MATRIX_HEIGHT), blocksize = BLOCKSIZE):
         self.size = {'width': size[0], 'height': size[1]}
-        self.blocksize = {'width': size[0], 'height': size[1]}
+        self.blocksize = blocksize
         self.surface = Surface((self.size['width'] * self.blocksize,
                                 (self.size['height']-2) * self.blocksize))
 
@@ -114,7 +114,7 @@ class Matris(object):
             elif pressed(pygame.K_UP) or pressed(pygame.K_w):
                 self.request_rotation()
 
-            elif pressed(pygame.K_LEFT) or pressed(pressed.K_a):
+            elif pressed(pygame.K_LEFT) or pressed(pygame.K_a):
                 self.request_movement('left')
                 self.movement_keys['left'] = 1
             elif pressed(pygame.K_RIGHT) or pressed(pygame.K_d):
@@ -147,7 +147,7 @@ class Matris(object):
         with_shadow = self.place_shadow()
 
         try:
-            with_tetromino = self.blend(self.totated(), allow_failure = False, matrix = with_shadow)
+            with_tetromino = self.blend(self.rotated(), allow_failure = False, matrix = with_shadow)
         except BrokenMatrixException:
             self.prepare_and_execute_gameover()
             return
@@ -170,7 +170,7 @@ class Matris(object):
         write_score(self.score)
         self.gameover = True
 
-    def palce_shadow(self):
+    def place_shadow(self):
         posY, posX = self.tetromino_position
         while self.blend(position = (posY, posX)):
             posY += 1
@@ -208,6 +208,12 @@ class Matris(object):
             return self.tetromino_rotation
         else:
             return False
+
+
+    def rotated(self, rotation=None):
+        if rotation is None:
+            rotation = self.tetromino_rotation
+        return rotate(self.current_tetromino.shape, rotation)
 
     def request_movement(self, direction):
         posY, posX = self.tetromino_position
@@ -350,8 +356,8 @@ class Game(object):
         background.blit(construct_nightmare(background.get_size()), (0, 0))
 
         self.matris = Matris()
-        matris_border = Surface(MATRIX_WIDTH * BLOCKSIZE + BORDERWIDTH * 2,
-                                VISIBLE_MATRIX_HEIGHT * BLOCKSIZE + BORDERWIDTH * 2)
+        matris_border = Surface((MATRIX_WIDTH * BLOCKSIZE + BORDERWIDTH * 2,
+                                VISIBLE_MATRIX_HEIGHT * BLOCKSIZE + BORDERWIDTH * 2))
         matris_border.fill(BORDERCOLOR)
 
         while 1:
@@ -382,7 +388,7 @@ class Game(object):
         textcolor = (255, 255, 255)
         font = pygame.font.Font(None, 30)
         width = (WIDTH - (MATRIS_OFFSET + BLOCKSIZE * MATRIX_WIDTH + 
-            BORDERWIDTH * 2)) - MARTRIS_OFFSET * 2
+            BORDERWIDTH * 2)) - MATRIS_OFFSET * 2
 
         def renderpair(text, val):
             text = font.render(text, True, textcolor)
@@ -422,7 +428,7 @@ class Game(object):
 
         return area
 
-    def next_tetromino_surf(self, next_tetromino_surf):
+    def next_tetromino_surf(self, tetromino_surf):
         area = Surface((BLOCKSIZE * 5, BLOCKSIZE * 5))
         area.fill(BORDERCOLOR)
         area.fill(BGCOLOR, Rect(BORDERWIDTH, BORDERWIDTH, 
@@ -461,6 +467,56 @@ class Menu(object):
             for event in events:
                 if event.type == pygame.QUIT:
                     exit()
+
+            menu.update(events, timepassed)
+
+            timepassed = clock.tick(30) / 1000.
+
+            if timepassed > 1:  # A game has most likely been played
+                highscoresurf = self.construct_highscoresurf()
+
+            screen.blit(nightmare, (0, 0))
+            screen.blit(highscoresurf, highscoresurf.get_rect(
+                            right = WIDTH - 50, bottom = HEIGHT - 50))
+            menu.draw(screen)
+            pygame.display.flip()
+
+    def construct_highscoresurf(self):
+        font = pygame.font.Font(None, 50)
+        hightscore = load_score()
+        text = "Highscore: {}".format(hightscore)
+        return font.render(text, True, (255, 255, 255))
+
+def construct_nightmare(size):
+    surf = Surface(size)
+
+    boxsize = 8
+    bordersize = 1
+    vals = '1235' # only the lower values, for darker colors and greater fear
+    arr = pygame.PixelArray(surf)
+    for x in xrange(0, len(arr), boxsize):
+        for y in xrange(0, len(arr[x]), boxsize):
+
+            color = int(''.join([random.choice(vals) + random.choice(vals) for 
+                            _ in range(3)]), 16)
+
+            for LX in xrange(x, x + (boxsize - bordersize)):
+                for LY in xrange(y, y + (boxsize - bordersize)):
+                    if LX < len(arr) and LY < len(arr[x]):
+                        arr[LX][LY] = color
+    del arr
+    return surf
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("MaTris")
+    Menu().main(screen)
+
+
+
+
+
 
 
 
